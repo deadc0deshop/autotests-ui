39a3e8878601a471506d570aa7fdcdb460b6ad84
deadc0deshop <kaboomqaa@gmail.com>
2026-08-26 11:01:45 +0300

    6.3 Маркировки Pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,9 @@
+import pytest
 from playwright.sync_api import sync_playwright
 
+
+@pytest.mark.registration
+@pytest.mark.regression
 def test_successful_registration():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
