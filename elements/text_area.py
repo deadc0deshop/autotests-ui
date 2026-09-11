92c9ce3a3945151d1767a20a38e31f06360ce404
deadc0deshop <kaboomqaa@gmail.com>
2026-09-11 11:02:28 +0300

    7.5 Знакомство с PageFactory

---
+++ elements/text_area.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+from elements.base_element import BaseElement
+
+class TextArea(BaseElement):
+    def get_locator(self, **kwargs):
+        return super().get_locator(**kwargs).locator('textarea').first
+
+    def fill(self, value: str, **kwargs):
+        locator = self.get_locator(**kwargs)
+        locator.fill(value)
+
+    def check_have_value(self, value: str, **kwargs):
+        locator = self.get_locator(**kwargs)
+        expect(locator).to_have_value(value)
